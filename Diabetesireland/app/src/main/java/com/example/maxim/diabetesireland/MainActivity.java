9e3d96e94434fd37e72fe9929b71aa374aaa6e34
Maksim Maiberg <maibergm@tcd.ie>
2016-02-16 12:56:15 +0000

    updated first screen

--- Diabetesireland/app/src/main/java/com/example/maxim/diabetesireland/MainActivity.java
+++ Diabetesireland/app/src/main/java/com/example/maxim/diabetesireland/MainActivity.java
@@ -2,62 +2,65 @@ package com.example.maxim.diabetesireland;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.Spinner;
-import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
 
-    private Spinner spinner;
-    private static final String[] paths = {"Gender", "Male", "Female"};
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    boolean touchedMale = false;
+    boolean touchedFemale = false;
+    Button maleButton;
+    Button femaleButton;
+    Button nextScreen;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nextScreen = (Button) findViewById(R.id.continueButton);
+        maleButton = (Button) findViewById(R.id.maleButton);
+        femaleButton = (Button) findViewById(R.id.femaleButton);
 
-        Button nextScreen = (Button) findViewById(R.id.continueButton);
-        nextScreen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Plan.class);
-                startActivity(intent);
-            }
-        });
+        maleButton.setOnClickListener(this);
+        femaleButton.setOnClickListener(this);
+        nextScreen.setOnClickListener(this);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.maleButton:
+                if (!touchedMale) {
+                    touchedFemale = false;
+                    touchedMale = true;
+                    femaleButton.setBackgroundColor(0xffadadad);//purple
+                    maleButton.setBackgroundColor(0xff432687); //grey
+                }
+                break;
+            case R.id.femaleButton:
+                if(!touchedFemale)
+                {
+                    touchedFemale = true;
+                    touchedMale = false;
+                    femaleButton.setBackgroundColor(0xff432687); //purple
+                    maleButton.setBackgroundColor(0xffadadad); //grey
+                }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+                break;
+            case R.id.continueButton:
+                Intent intent = new Intent(MainActivity.this, Plan.class);
+                startActivity(intent);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                break;
+
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
