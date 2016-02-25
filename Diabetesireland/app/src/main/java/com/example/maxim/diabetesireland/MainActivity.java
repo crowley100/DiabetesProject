52149eb6eaac000aed1ad539a755dc5e766d3af1
Maksim Maiberg <maibergm@tcd.ie>
2016-02-25 13:44:36 +0000

    Changes to main screen, only need to add date picker, and preferably change the background colour to white of the drop down menus, not working for me though

--- Diabetesireland/app/src/main/java/com/example/maxim/diabetesireland/MainActivity.java
+++ Diabetesireland/app/src/main/java/com/example/maxim/diabetesireland/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.maxim.diabetesireland;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Window;
@@ -14,6 +16,10 @@ import android.widget.Spinner;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     boolean touchedMale = false;
     boolean touchedFemale = false;
+    Spinner weight;
+    Spinner height;
+    String[] heights = new String[] {"Metres","<1.40m","1.4-1.5m", "1.5-1.6m", "1.6-1.7m", "1.7-1.8m","1.8m-1.9m", "1.9-2m", "2m+"};
+    String[] weights = new String[]{"Kilograms","<60kg", "60-65kg", "65-70kg","70-75kg","75-80kg","80-85kg","85-90kg","90-95kg","95-100kg","100-110kg","110-120kg","120-130kg","130-140kg","140-150kg","150k-200kg", "200+kg"};
     Button maleButton;
     Button femaleButton;
     Button nextScreen;
@@ -25,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         nextScreen = (Button) findViewById(R.id.continueButton);
         maleButton = (Button) findViewById(R.id.maleButton);
         femaleButton = (Button) findViewById(R.id.femaleButton);
@@ -32,6 +39,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         maleButton.setOnClickListener(this);
         femaleButton.setOnClickListener(this);
         nextScreen.setOnClickListener(this);
+
+        weight = (Spinner)findViewById(R.id.weightSpinner);
+        height = (Spinner)findViewById(R.id.hSpinner);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_spinner_dropdown_item,heights);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<String>adapter2 = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_spinner_dropdown_item,weights);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        weight.setAdapter(adapter2);
+        height.setAdapter(adapter);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -47,7 +67,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     maleButton.setBackgroundColor(0xff432687); //purple
                     maleButton.setTextColor(0xffffffff); //white
                     femaleButton.setBackgroundColor(0xffdbdbdb); //light grey
-                    femaleButton.setTextColor(0xffadadad); //dark grey
+                    femaleButton.setTextColor(0xffffffff); //dark grey
                 }
                 break;
             case R.id.femaleButton:
@@ -58,7 +78,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     femaleButton.setBackgroundColor(0xff432687);//purple
                     femaleButton.setTextColor(0xffffffff); // white text
                     maleButton.setBackgroundColor(0xffdbdbdb); // dark grey background
-                    maleButton.setTextColor(0xffadadad); //light grey text
+                    maleButton.setTextColor(0xffffffff); //light grey text
                 }
                 break;
             case R.id.continueButton:
