04a5ad57a0bd6bb3a28458501d0a2993b8fb8c7f
Maksim Maiberg <maibergm@tcd.ie>
2016-02-25 18:30:56 +0000

    UI elements of front page basically finished

--- Diabetesireland/app/src/main/java/com/example/maxim/diabetesireland/MainActivity.java
+++ Diabetesireland/app/src/main/java/com/example/maxim/diabetesireland/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.maxim.diabetesireland;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
@@ -10,21 +12,28 @@ import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.RadioButton;
 import android.widget.Spinner;
+
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     boolean touchedMale = false;
     boolean touchedFemale = false;
+    Calendar calendar = Calendar.getInstance();
     Spinner weight;
     Spinner height;
-    String[] heights = new String[] {"Metres","<1.40m","1.4-1.5m", "1.5-1.6m", "1.6-1.7m", "1.7-1.8m","1.8m-1.9m", "1.9-2m", "2m+"};
-    String[] weights = new String[]{"Kilograms","<60kg", "60-65kg", "65-70kg","70-75kg","75-80kg","80-85kg","85-90kg","90-95kg","95-100kg","100-110kg","110-120kg","120-130kg","130-140kg","140-150kg","150k-200kg", "200+kg"};
+    String[] heights = new String[]{"Metres", "<1.40m", "1.4-1.5m", "1.5-1.6m", "1.6-1.7m", "1.7-1.8m", "1.8m-1.9m", "1.9-2m", "2m+"};
+    String[] weights = new String[]{"Kilograms", "<60kg", "60-65kg", "65-70kg", "70-75kg", "75-80kg", "80-85kg", "85-90kg", "90-95kg", "95-100kg", "100-110kg", "110-120kg", "120-130kg", "130-140kg", "140-150kg", "150k-200kg", "200+kg"};
     Button maleButton;
     Button femaleButton;
+    Button datePicker;
+    RadioButton weightButton;
+    RadioButton activeButton;
     Button nextScreen;
     Intent intent;
-
 
 
     @Override
@@ -35,19 +44,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         nextScreen = (Button) findViewById(R.id.continueButton);
         maleButton = (Button) findViewById(R.id.maleButton);
         femaleButton = (Button) findViewById(R.id.femaleButton);
-
+        weightButton = (RadioButton) findViewById(R.id.weightRadio);
+        activeButton = (RadioButton) findViewById(R.id.activeRadio);
+        datePicker = (Button) findViewById(R.id.datePicker);
+        datePicker.setOnClickListener(this);
         maleButton.setOnClickListener(this);
         femaleButton.setOnClickListener(this);
         nextScreen.setOnClickListener(this);
-
-        weight = (Spinner)findViewById(R.id.weightSpinner);
-        height = (Spinner)findViewById(R.id.hSpinner);
-        ArrayAdapter<String>adapter = new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_spinner_dropdown_item,heights);
+        //DropDowns
+        weight = (Spinner) findViewById(R.id.weightSpinner);
+        height = (Spinner) findViewById(R.id.hSpinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_spinner_dropdown_item, heights);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        ArrayAdapter<String>adapter2 = new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_spinner_dropdown_item,weights);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_spinner_dropdown_item, weights);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         weight.setAdapter(adapter2);
@@ -57,9 +69,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setSupportActionBar(toolbar);
     }
 
+
+
     @Override
     public void onClick(View v) {
-        switch(v.getId()) {
+        switch (v.getId()) {
             case R.id.maleButton:
                 if (!touchedMale) {
                     touchedFemale = false;
@@ -71,8 +85,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
             case R.id.femaleButton:
-                if(!touchedFemale)
-                {
+                if (!touchedFemale) {
                     touchedFemale = true;
                     touchedMale = false;
                     femaleButton.setBackgroundColor(0xff432687);//purple
@@ -85,8 +98,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 intent = new Intent(MainActivity.this, Triangle.class);
                 startActivity(intent);
                 break;
+            case R.id.datePicker:
+                new DatePickerDialog(MainActivity.this, listener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+                break;
 
 
         }
     }
+
+    DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+            datePicker.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+        }
+
+
+    };
 }
+
+
